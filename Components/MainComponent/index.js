6a8f29a6619720dad52717cd6077cc9182d69b54
andrelara2002 <andrelara2002@gmail.com>
2020-11-25 21:19:06 -0300

    2 files modified

--- Components/MainComponent/index.js
+++ Components/MainComponent/index.js
@@ -4,30 +4,67 @@ import "./styles.css";
 class MainComponent extends Component {
   constructor(props) {
     super();
+    this.spaceVariables = {
+      unit: ["px", "rem"]
+    };
     this.state = {
-      width: "20rem",
-      height: "20rem",
-      top_left: "10px",
-      top_right: "10px",
-      bottom_left: "10px",
-      bottom_right: "10px"
+      size: "20rem",
+      top_left: null,
+      top_right: null,
+      bottom_left: null,
+      bottom_right: null
     };
   }
 
   componentDidMount() {
-    this.generateSquare();
+    this.getInputsValues();
   }
 
+  getInputsValues = () => {
+    let topLeft = document.getElementById("topLeft");
+    let topRight = document.getElementById("topRight");
+    let bottomLeft = document.getElementById("bottomLeft");
+    let bottomRight = document.getElementById("bottomRight");
+
+    if (
+      isNaN(topLeft.value) ||
+      isNaN(topRight.value) ||
+      isNaN(bottomLeft.value) ||
+      isNaN(bottomRight.value)
+    ) {
+      alert("Por favor, insira um número");
+    } else {
+      this.setState(
+        {
+          size: this.state.size,
+          top_left: topLeft.value,
+          top_right: topRight.value,
+          bottom_left: bottomLeft.value,
+          bottom_right: bottomRight.value
+        },
+        () => {
+          this.generateSquare();
+        }
+      );
+    }
+  };
+
   generateSquare = () => {
+    let actualUnit = "";
+
+    if (1 === 1) {
+      actualUnit = this.spaceVariables.unit[0];
+    }
+
     const square = document.getElementById("squareElement");
-    square.style.width = this.state.width;
-    square.style.height = this.state.height;
+    square.style.width = this.state.size;
+    square.style.height = this.state.size;
 
-    square.style.borderTopLeftRadius = this.state.top_left;
-    square.style.borderTopRightRadius = this.state.top_right;
+    square.style.borderTopLeftRadius = this.state.top_left + actualUnit;
+    square.style.borderTopRightRadius = this.state.top_right + actualUnit;
 
-    square.style.borderBottomLeftRadius = this.state.bottom_left;
-    square.style.borderBottomRightRadius = this.state.bottom_right;
+    square.style.borderBottomLeftRadius = this.state.bottom_left + actualUnit;
+    square.style.borderBottomRightRadius = this.state.bottom_right + actualUnit;
     square.style.border = "1px solid black";
   };
 
@@ -36,10 +73,10 @@ class MainComponent extends Component {
       {
         width: this.state.width,
         height: this.state.height,
-        top_left: "4rem",
-        top_right: "4rem",
-        bottom_left: "2rem",
-        bottom_right: "2rem"
+        top_left: this.state.top_left,
+        top_right: this.state.top_right,
+        bottom_left: this.state.bottom_left,
+        bottom_right: this.state.bottom_right
       },
       () => {
         this.generateSquare();
@@ -52,14 +89,18 @@ class MainComponent extends Component {
       <div className="main">
         <div className="square" id="squareElement" />
         <div className="inputForm">
-          <label for="topLeft">Top Left </label>
-          <input name="topLeft" />
-          <label for="topRight">Top Right </label>
-          <input name="topRight" />
-          <label for="bottomLeft">Bottom Left </label>
-          <input name="bottomLeft" />
-          <label for="bottomRight">Bottom Right</label>
-          <input name="bottomRight" />
+          <div className="inputDiv">
+            <input id="topLeft" value={10} />
+          </div>
+          <div className="inputDiv">
+            <input id="topRight" value={10} />
+          </div>
+          <div className="inputDiv">
+            <input id="bottomLeft" value={10} />
+          </div>
+          <div className="inputDiv">
+            <input id="bottomRight" value={10} />
+          </div>
         </div>
         <button onClick={() => this.setSquareBorders()}> clique aqui </button>
       </div>
