621d448aa0f8c627f44860901cbecd27a0a8e8b9
andrelara2002 <andrelara2002@gmail.com>
2020-11-25 22:13:37 -0300

    2 files modified

--- Components/MainComponent/index.js
+++ Components/MainComponent/index.js
@@ -5,14 +5,13 @@ class MainComponent extends Component {
   constructor(props) {
     super();
     this.spaceVariables = {
-      unit: ["px", "rem"]
+      unit: "px"
     };
     this.state = {
-      size: "20rem",
-      top_left: null,
-      top_right: null,
-      bottom_left: null,
-      bottom_right: null
+      top_left: 10,
+      top_right: 10,
+      bottom_left: 10,
+      bottom_right: 10
     };
   }
 
@@ -49,23 +48,36 @@ class MainComponent extends Component {
     }
   };
 
-  generateSquare = () => {
-    let actualUnit = "";
+  setButtonColors = () => {
+    const buttonElementPx = document.getElementById("pxButton");
+    const buttonElementRem = document.getElementById("remButton");
 
-    if (1 === 1) {
-      actualUnit = this.spaceVariables.unit[0];
+    if (this.spaceVariables.unit === "px") {
+      buttonElementPx.style.borderColor = "#07d962";
+      buttonElementPx.style.background = "#19181f";
+
+      buttonElementRem.style.background = "#67159c";
+      buttonElementRem.style.borderColor = "#67159c";
+    } else {
+      buttonElementRem.style.borderColor = "#07d962";
+      buttonElementRem.style.background = "#19181f";
+
+      buttonElementPx.style.background = "#67159c";
+      buttonElementPx.style.borderColor = "#67159c";
     }
+  };
+
+  generateSquare = () => {
+    let actualUnit = this.spaceVariables.unit;
+    this.setButtonColors();
 
     const square = document.getElementById("squareElement");
-    square.style.width = this.state.size;
-    square.style.height = this.state.size;
 
     square.style.borderTopLeftRadius = this.state.top_left + actualUnit;
     square.style.borderTopRightRadius = this.state.top_right + actualUnit;
 
     square.style.borderBottomLeftRadius = this.state.bottom_left + actualUnit;
     square.style.borderBottomRightRadius = this.state.bottom_right + actualUnit;
-    square.style.border = "1px solid black";
   };
 
   setSquareBorders = () => {
@@ -90,19 +102,42 @@ class MainComponent extends Component {
         <div className="square" id="squareElement" />
         <div className="inputForm">
           <div className="inputDiv">
-            <input id="topLeft" value={10} />
+            <input id="topLeft" />
           </div>
           <div className="inputDiv">
-            <input id="topRight" value={10} />
+            <input id="topRight" />
           </div>
           <div className="inputDiv">
-            <input id="bottomLeft" value={10} />
+            <input id="bottomLeft" />
           </div>
           <div className="inputDiv">
-            <input id="bottomRight" value={10} />
+            <input id="bottomRight" />
           </div>
         </div>
-        <button onClick={() => this.setSquareBorders()}> clique aqui </button>
+        <div className="buttonDiv">
+          <button onClick={() => this.getInputsValues()}>Gerar</button>
+          <button>Limpar</button>
+          <div className="unitButton">
+            <button
+              onClick={() => {
+                this.spaceVariables.unit = "rem";
+                this.generateSquare();
+              }}
+              id="remButton"
+            >
+              REM
+            </button>
+            <button
+              onClick={() => {
+                this.spaceVariables.unit = "px";
+                this.generateSquare();
+              }}
+              id="pxButton"
+            >
+              PX
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
